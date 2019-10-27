2d980a75d77b2d1ec721c005e7e8b2bbf2e4fcf1
YPAzevedo <yago.pereira.az@gmail.com>
2019-10-27 18:47:15 -0300

    Updates

---
+++ precache-manifest.7a186bc8c50ba04b4fb68bf0a6a708fc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ab9287645c6b4b6f71c8b53ca2dafa1",
+    "url": "/Github-issues-app/index.html"
+  },
+  {
+    "revision": "1d84b79d8ddfabc0e954",
+    "url": "/Github-issues-app/static/js/2.9d4f419f.chunk.js"
+  },
+  {
+    "revision": "d986e733a7988c85265e",
+    "url": "/Github-issues-app/static/js/main.1a5cd15a.chunk.js"
+  },
+  {
+    "revision": "6f73273a14671111da11",
+    "url": "/Github-issues-app/static/js/runtime-main.6c5ec49f.js"
+  }
+]);
